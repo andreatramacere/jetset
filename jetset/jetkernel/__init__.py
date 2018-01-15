f97f6d766e93dc9d858a929dc2bcb107c9c889f5
andrea tramacere <andrea.tramacere@gmail.com>
2018-01-15 16:42:53 +0100

    removed verbose package_dir msg, breaing web sed tool

--- jetset/jetkernel/__init__.py
+++ jetset/jetkernel/__init__.py
@@ -4,4 +4,4 @@ import jetkernel
 #package_dir=os.path.dirname(BlazarSED.__file__)
 
 os.environ['BLAZARSED']=os.path.dirname(os.path.abspath(__file__))
-print 'package_dir',os.path.dirname(os.path.abspath(__file__))
+#print 'package_dir',os.path.dirname(os.path.abspath(__file__))
