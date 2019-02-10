80cdf3a377c57f21a7910aa1e24a8431e74d59dd
andrea tramacere <orion@MacBook-Pro-3.local>
2019-02-10 17:31:40 +0100

    update versioning

--- make_tag.py
+++ make_tag.py
@@ -66,7 +66,7 @@ def do_remote_push(message):
     l.append(["git add . "])
     l.append(['''git commit -m '%s' ''' % (message)])
     l.append(['git push origin py23'])
-    l.append(['git push origin-github py23 %s'])
+    l.append(['git push origin-github py23'])
 
     res = subprocess.call(l[0], shell=True)
     for  i in l[1::]:
