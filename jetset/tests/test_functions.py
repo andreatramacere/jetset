fb0f6caf635c695b7821ffe28f630739eafa25af
andrea tramacere <orion@MacBook-Pro-3.local>
2020-01-13 22:36:19 +0100

    updated tests

--- jetset/tests/test_functions.py
+++ jetset/tests/test_functions.py
@@ -7,7 +7,7 @@ def data():
     from jetset.data_loader import ObsData, Data
     from jetset.test_data_helper import test_SEDs
 
-    data=Data.from_file(test_SEDs[1])
+    data=Data.from_file(test_SEDs[2])
     sed_data=ObsData(data_table=data)
     sed_data.group_data(bin_width=0.2)
 
@@ -32,7 +32,7 @@ def spectral_indices(sed_data):
 
 def sed_shaper(my_shape):
 
-    mm, best_fit = my_shape.sync_fit(check_host_gal_template=False,
+    mm, best_fit = my_shape.sync_fit(check_host_gal_template=True,
                                      Ep_start=None,
                                      minimizer='lsb',
                                      silent=True,
@@ -56,7 +56,7 @@ def model_constr(my_shape):
                               B_range=[0.001, 0.1],
                               distr_e='lppl',
                               t_var_sec=3 * 86400,
-                              nu_cut_IR=1E12,
+                              nu_cut_IR=1E11,
                               SEDShape=my_shape)
 
     prefit_jet = sed_obspar.constrain_SSC_model(electron_distribution_log_values=False)
@@ -64,41 +64,50 @@ def model_constr(my_shape):
 
     return prefit_jet
 
-def model_fit_lsb(sed_data,):
+def model_fit_lsb(sed_data,my_shape):
     from jetset.minimizer import fit_SED
     from jetset.model_manager import FitModel
     from jetset.jet_model import Jet
 
-    jet = Jet.load_model('prefit_jet.dat')
+    jet_lsb = Jet.load_model('prefit_jet_gal_templ.dat')
+    jet_lsb.set_gamma_grid_size(200)
 
-    fit_model = FitModel(jet=jet, name='SSC-best-fit', template=None)
-    fit_model.freeze('z_cosm')
-    fit_model.freeze('R_H')
-    fit_model.parameters.R.fit_range = [10 ** 15.5, 10 ** 17.5]
-    fit_model.parameters.gmax.fit_range = [1E4, 1E8]
-    model_minimizer_lsb, best_fit_lsb = fit_SED(fit_model, sed_data, 10.0 ** 11, 10 ** 29.0, fitname='SSC-best-fit',
-                                                minimizer='lsb')
-    best_fit_lsb.save_report('best-fit-lsb-report.txt')
-    fit_model.save_model('fit_model_lsb.dat')
+    fit_model_lsb = FitModel(jet=jet_lsb, name='SSC-best-fit-lsb', template=my_shape.host_gal)
+    fit_model_lsb.freeze('z_cosm')
+    fit_model_lsb.freeze('R_H')
+    fit_model_lsb.parameters.beam_obj.fit_range = [5, 50]
+    fit_model_lsb.parameters.R.fit_range = [10 ** 15.5, 10 ** 17.5]
+    fit_model_lsb.parameters.gmax.fit_range = [1E4, 1E8]
+    fit_model_lsb.parameters.nuFnu_p_host.frozen = False
+    fit_model_lsb.parameters.nu_scale.frozen = True
 
-def model_fit_minuit(sed_data):
+    model_minimizer_lsb, best_fit_lsb = fit_SED(fit_model_lsb, sed_data, 10.0 ** 11, 10 ** 29.0,
+                                                fitname='SSC-best-fit-lsb', minimizer='lsb')
+
+    model_minimizer_lsb.save_report('best-fit-minuit-report.txt')
+    best_fit_lsb.save_model('fit_model_lsb.dat')
+
+def model_fit_minuit(sed_data,my_shape):
     from jetset.minimizer import fit_SED
     from jetset.model_manager import FitModel
     from jetset.jet_model import Jet
 
-    jet = Jet.load_model('prefit_jet.dat')
+    jet_minuit = Jet.load_model('prefit_jet_gal_templ.dat')
+    jet_minuit.set_gamma_grid_size(200)
 
-    fit_model = FitModel(jet=jet, name='SSC-best-fit', template=None)
-    fit_model.freeze('z_cosm')
-    fit_model.freeze('R_H')
-    fit_model.parameters.R.fit_range = [10 ** 15.5, 10 ** 17.5]
-    fit_model.parameters.gmax.fit_range = [1E4, 1E8]
+    fit_model_minuit = FitModel(jet=jet_minuit, name='SSC-best-fit-minuit', template=my_shape.host_gal)
+    fit_model_minuit.freeze('z_cosm')
+    fit_model_minuit.freeze('R_H')
+    fit_model_minuit.parameters.beam_obj.fit_range = [5, 50]
+    fit_model_minuit.parameters.R.fit_range = [10 ** 15.5, 10 ** 17.5]
+    fit_model_minuit.parameters.nuFnu_p_host.frozen = False
+    fit_model_minuit.parameters.nu_scale.frozen = True
 
-    model_minimizer_minuit, best_fit_minuit = fit_SED(fit_model, sed_data, 10.0 ** 11, 10 ** 29.0,
-                                                      fitname='SSC-best-fit', minimizer='minuit', loglog=False)
+    model_minimizer_minuit, best_fit_minuit = fit_SED(fit_model_minuit, sed_data, 10.0 ** 11, 10 ** 29.0,
+                                                      fitname='SSC-best-fit-minuit', minimizer='minuit')
 
     best_fit_minuit.save_report('best-fit-minuit-report.txt')
-    fit_model.save_model('fit_model_minuit.dat')
+    fit_model_minuit.save_model('fit_model_minuit.dat')
 
 def test_full():
 
