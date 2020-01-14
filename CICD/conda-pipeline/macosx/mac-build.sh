96475e8ed6bd0565a2bc4ecb063fbc2f1891a496
andrea tramacere <orion@MacBook-Pro-3.local>
2020-01-14 18:05:55 +0100

    updated conda-pipeline

--- CICD/conda-pipeline/macosx/mac-build.sh
+++ CICD/conda-pipeline/macosx/mac-build.sh
@@ -17,8 +17,8 @@ cd CICD/conda-pipeline/macosx
 
 conda create --yes --name jetset-cidc python=3.7 ipython anaconda-client conda-build ipython
 conda activate jetset-cidc
-export PKG_VERSION=$(cd ../../ && python -c "import jetset;print(jetset.__version__)")
-rm -rf ../../jetset/__pycache__/
+export PKG_VERSION=$(cd ../../../ && python -c "import jetset;print(jetset.__version__)")
+rm -rf ../../../jetset/__pycache__/
 echo  $PKG_VERSION
 
 #now using env var
