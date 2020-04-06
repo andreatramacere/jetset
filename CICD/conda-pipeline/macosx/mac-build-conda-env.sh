624a41c4bad0876688e8beeb3ec6e64ebadac313
andrea tramacere <orion@MacBook-Pro-3.local>
2020-04-06 03:55:28 +0200

    update cicd mac

--- CICD/conda-pipeline/macosx/mac-build-conda-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo  '>>>>>>>>>>>>>>>>>>>>>>>>>>> BUILD  jetset-cidc env <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<',$PWD
-conda create --yes --name jetset-cidc python=3.7 ipython anaconda-client conda-build ipython>conda_env_build.log
