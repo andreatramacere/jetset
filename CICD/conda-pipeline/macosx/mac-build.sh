99c162074f47ddd329596fed9e6c6cee3df0dce9
andrea tramacere <orion@MacBook-Pro-3.local>
2020-04-06 03:55:01 +0200

    update cicd mac

--- CICD/conda-pipeline/macosx/mac-build.sh
+++ CICD/conda-pipeline/macosx/mac-build.sh
@@ -12,6 +12,7 @@ python setup.py clean
 cd CICD/conda-pipeline/macosx
 
 echo  '>>>>>>>>>>>>>>>>>>>>>>>>>>> BUILD  jetset-cidc env <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<',$PWD
+conda create --yes --name jetset-cidc python=3.7 ipython anaconda-client conda-build ipython>conda_env_build.log
 conda activate jetset-cidc
 conda install --yes   -c astropy -c conda-forge --file ../../../requirements.txt
 
