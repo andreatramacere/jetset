0340649e94d281cb281b3ee381934ddd08580ab8
andreatramacere <andrea.tramacere@gmail.com>
2021-01-27 23:21:20 +0100

    update conda workflow

--- CICD/conda-pipeline/manual/macosx/mac-update-git.sh
+++
@@ -1,9 +0,0 @@
-echo  '>>>>>>>>>>>>>>>>>>>>>>>>>>> git  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-
-cd integration
-git clone https://github.com/andreatramacere/jetset.git
-cd jetset
-git checkout develop
-git reset --hard HEAD
-git pull origin develop
-cd ../../
