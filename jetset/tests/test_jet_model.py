b40c89f8a7801c24e4f01730c4d55ff64d1a1595
andreatramacere <andrea.tramacere@gmail.com>
2022-05-02 21:35:38 +0200

    update tests

--- jetset/tests/test_jet_model.py
+++ jetset/tests/test_jet_model.py
@@ -8,7 +8,22 @@ class TestJets(TestBase):
     def integration_suite(self,plot=False):
         self._all(plot=plot)
 
-    
+    def test_hadronic_jet(self,plot=False):
+        print('--------> test_hadronic_jet',plot)
+        from jetset.jet_model import Jet
+        j = Jet(proton_distribution='plc')
+        j.parameters.gmin.val = 2
+        j.parameters.gmax.val = 1E8
+        j.parameters.NH_pp.val = 1E10
+        j.parameters.N.val = 1E1
+        j.parameters.B.val = 80
+
+        j.parameters.p.val = 2.5
+        j.eval()
+        j.show_model()
+
+        j.eval()
+        
 
     def test_build_bessel(self,plot=False):
         print('--------> test_build_bessel',plot)
