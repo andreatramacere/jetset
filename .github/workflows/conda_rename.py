e71b136ccef2f8530ff00ff703b10342e4f12e6c
andreatramacere <andrea.tramacere@gmail.com>
2023-09-15 13:11:34 +0200

    update workflows

---
+++ .github/workflows/conda_rename.py
@@ -0,0 +1,17 @@
+import os
+import platform
+import shutil
+name=os.getenv('CONDABUILDJETSET')
+
+if platform.system()=='Darwin':
+    system_str = 'macos'
+else:
+    system_str = 'linux'
+
+new_name=name.replace('.tar.bz2','_%s.tar.bz2'%system_str)
+
+shutil.move(name,new_name)
+
+os.makedir('conda-binary')
+
+shutil.move(new_name,'conda-binary')
