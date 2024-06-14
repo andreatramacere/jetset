0b2983c5f1f337d2899f1eeb3b4425dc8bf3e4f4
andreatramacere <andrea.tramacere@gmail.com>
2024-06-15 00:55:58 +0200

    update doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ import sys
 import os
 import json
 import mock
-import jetset
+#import jetset
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
