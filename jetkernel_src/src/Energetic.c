bbca36699f6e485475426dedc1abfbfffdcf0344
andreatramacere <andrea.tramacere@gmail.com>
2025-01-05 22:06:56 +0100

    added comment

--- jetkernel_src/src/Energetic.c
+++ jetkernel_src/src/Energetic.c
@@ -391,6 +391,7 @@ struct jet_energetic EnergeticOutput(struct blob * pt) {
     //char f_Energetic[static_file_name_max_legth];
     //FILE *fp_Energetic;
 
+    //Lum factor consistent with Eq. 3 and 4, Ghisellini 2010, doi:10.1111/j.1365-2966.2009.15898.x
     lum_factor_rad =0.25 *eval_beta_gamma(pt->BulkFactor) * pt->BulkFactor * pt->BulkFactor ;
     lum_factor = pi * pt->R * pt->R * vluce_cm * eval_beta_gamma(pt->BulkFactor) * pt->BulkFactor * pt->BulkFactor ;
     energetic.U_B= pt->UB;
