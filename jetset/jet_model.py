0843df7eb9e08e559a3542ad7cdf8b6163ff768e
andrea tramacere <andrea.tramacere@gmail.com>
2025-09-27 14:56:08 +0200

    fixing nu size for no int abs.

--- jetset/jet_model.py
+++ jetset/jet_model.py
@@ -222,7 +222,7 @@ class JetBase(Model):
     def __setstate__(self,state):
         self.__init__()
         self._decode_model(state)
-        self._fix_par_dep_on_load()
+        self._fix_par_dep_on_load(verbose=False)
         if '_internal_absorption_comp' in state:
             self._internal_absorption_comp=state['_internal_absorption_comp']
         #    for c in state['_internal_absorption_comp'].keys():
@@ -1536,7 +1536,7 @@ class JetBase(Model):
         if self.emitters_distribution is None:
             raise RuntimeError('emitters distribution not defined')
 
-        tau_tot=np.zeros(lin_nu.shape)
+        tau_tot=np.zeros(np.size(lin_nu.shape))
         for  iac in self._internal_absorption_comp.keys():
                 int_abs=self._internal_absorption_comp[iac]['method']
                 tau_c,nu_src=int_abs.eval(get_tau=True,lin_nu=lin_nu)
