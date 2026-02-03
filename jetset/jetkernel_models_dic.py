069ee017af6add297b8f7f4a1b73108ddff41a73
andrea tramacere <andrea.tramacere@gmail.com>
2026-02-03 13:39:13 +0100

    use Ne/p_jetset in place of Ne/p in gamma_dic_e/p

--- jetset/jetkernel_models_dic.py
+++ jetset/jetkernel_models_dic.py
@@ -90,9 +90,9 @@ n_seed_dic['Star']=['n_Star','nu_Sync']
 
 #Electron distributions
 
-gamma_dic_e={'electron_distr':['Ne','griglia_gamma_Ne_log']}
+gamma_dic_e={'electron_distr':['Ne_jetset','griglia_gamma_Ne_log']}
 
-gamma_dic_p={'proton_distr':['Np','griglia_gamma_Np_log']}
+gamma_dic_p={'proton_distr':['Np_jetset','griglia_gamma_Np_log']}
 gamma_dic_pp_e_second={'e_second_inj':['Q_inj_e_second','griglia_gamma_Ne_log']}
 
 s_dic={'pl':'p'}
