67d49d1910cf2daea19627892ea951c4efc4188e
andrea tramacere <andrea.tramacere@gmail.com>
2026-02-28 18:08:04 +0100

    fixed numpy private/public API import

--- jetset/jet_spectral_components.py
+++ jetset/jet_spectral_components.py
@@ -5,7 +5,9 @@ __author__ = "Andrea Tramacere"
 import numpy as np
 import os
 from astropy.table import Table
-from numpy.core._multiarray_umath import zeros, log10
+#TODO: double check the log10 import public vs private numpy API
+#from numpy.core._multiarray_umath impo
+from numpy import log10
 from scipy import interpolate
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
