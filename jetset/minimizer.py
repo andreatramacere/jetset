0fc9641d6b5d61dbe197abf66db6de448aed808f
andrea tramacere <andrea.tramacere@gmail.com>
2026-03-07 18:29:35 +0100

    removed unsused jetset/leastsqbound

--- jetset/minimizer.py
+++ jetset/minimizer.py
@@ -28,9 +28,8 @@ from tqdm.auto import tqdm
 
 from .plot_sedfit import plt,heatmap,annotate_heatmap
 
-from scipy.optimize import least_squares,minimize
+from scipy.optimize import least_squares
 from scipy import interpolate
-from .leastsqbound.leastsqbound import leastsqbound
 
 from .output import section_separator,WorkPlace,makedir
 from .utils import JetkerneltException
