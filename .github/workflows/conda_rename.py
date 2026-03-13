6c2a40930fb1acfd1c85e28dc74a74459ed449ba
andrea tramacere <andrea.tramacere@gmail.com>
2026-03-13 17:29:53 +0100

    updated conda_rename.py

--- .github/workflows/conda_rename.py
+++ .github/workflows/conda_rename.py
@@ -11,7 +11,7 @@ else:
 if 'arm64' in platform.platform():
     system_str= '%s_arm64'%system_str
 
-new_name=name.replace('.tar.bz2','_conda_%s.tar.bz2'%system_str)
+new_name=name.replace('.conda','_%s.conda'%system_str)
 
 shutil.move(name,new_name)
 
